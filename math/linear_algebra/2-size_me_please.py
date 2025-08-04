3f567d9d36ba614703af8fc23a0958d861fce50a
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-04 11:06:34 +0200

    add docs strings to 2-size_me_please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """matrix_shape
+
+    Calcule les dimensions d'une matrice (liste imbriquée).
+
+    Args:
+        matrix (list): Liste imbriquée 1D, 2D ou 3D...
+
+    Liste les dimensions de la matrice dans l'ordre suivant:
+        Si 3D : nombre de blocs (Z axis), nombre de lignes (Y axis), nombre de colonnes (X axis)
+        Si 2D : nombre de lignes (Y axis), nombre de colonnes (X axis)
+
+    Returns:
+        list: taille de la matrice donnée
+    """
     shape = []
     while isinstance(matrix, list):  # Tant que matrix est une liste
         shape.append(len(matrix))  # Ajoute la taille de matrix
