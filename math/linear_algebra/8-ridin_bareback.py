588d8e269ab7b442bbaa5eb3d938ac16f8b36b2e
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-04 19:56:31 +0200

    fix 8-ridin_bareback, the previous code assumes that both mat1 rows have exactly 2 elements and that mat2 has exactly 2 rows. The code didn't work if matrices with differents size is used. To fix this i add a new loop trough len(mat2) to always have the right numbers of iteraction in the elements. Also add the condition to ensure the multiplication is possible

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -2,6 +2,15 @@
 """
 Function that performs matrix multiplication by doing the
 multiplication of the index line of mat1 with columns index of mat2
+
+exemple:
+mat1 = [[1, 2],
+        [3, 4]]
+
+mat2 = [[5, 6],
+        [7, 8]]
+>>> [[19, 22],  -> (1*5 + 2*7), (1*6 + 2*8)
+>>> [43, 50]]   -> (3*5 + 4*7), (3*6 + 4*8)
 """
 
 
@@ -17,19 +26,20 @@ def mat_mul(mat1, mat2):
     """
     new = []
 
+    if len(mat1[0]) != len(mat2):
+        return None
+
     for i in range(len(mat1)):  # Parcourt les lignes de mat1
         ligne = []  # Reinitialise lignes
         for y in range(len(mat2[0])):  # Parcourt les colonnes de mat2
-            # Calcule horrible
-            val = (mat1[i][0] * mat2[0][y] + mat1[i][1] * mat2[1][y])
-            # print(f"calcul. mat1[{i}][0] * mat2[0][{y}]
-            #              + mat1[{i}][1] * mat2[1][{y}]")
-            # print(f"1. {mat1[i][0]} * {mat2[0][y]}
-            #               + {mat1[i][1]} * {mat2[1][y]}")
-            # print(f"2. {mat1[i][0] * mat2[0][y]}
-            #               + {mat1[i][1] * mat2[1][y]}")
-            # print(f"3. {mat1[i][0] * mat2[0][y]
-            #                + mat1[i][1] * mat2[1][y]}")
-            ligne.append(val)  # ajoute les valeurs dans le tableau de ligne
-        new.append(ligne)  # Ajoute la ligne au tableau final
+            val = 0  # Reinitialise val
+            for k in range(len(mat2)):  # Parcourt les lignes de mat2
+                # print(f"val{k} += mat1[{i}][{k}] * mat2[{k}][{y}]
+                #           or val += {mat1[i][k]} * {mat2[k][y]}")
+                # print(f"val calcul {val} + {mat1[i][k] * mat2[k][y]}")
+                val += mat1[i][k] * mat2[k][y]
+                # print(f"val = {val}")
+            # print(f"result = {val}")
+            ligne.append(val)
+        new.append(ligne)
     return new
