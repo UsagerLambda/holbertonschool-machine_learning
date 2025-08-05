0193a929682d17b689166c5c6d31b266d6801a16
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-05 08:57:12 +0200

    add 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Function that return the shape of a given matrice
+"""
+
+
+def np_shape(matrix):
+    """np_shape
+
+    Args:
+        matrix (list): matrice of integers
+
+    Returns:
+        tuple: the shape of the given matrice
+    """
+    new = matrix.shape
+    return new
