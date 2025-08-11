1a1e7b2da7c603927efe41496bd716b4c8b16223
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-11 16:38:23 +0200

    fix 6-bars.py, im dumb, i didn't see i have to display the total number of fruit for each person, so each bars stand on the top of the previous one by starting at the value the previous stopped

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -30,17 +30,24 @@ def bars():
 
     x = ["Farrah", "Fred", "Felicia"]
 
-    plt.bar(x, fruit[3], bottom=fruit[2], color='#ffe5b4',
-            width=0.5, label="Peaches")
-    plt.bar(x, fruit[2], bottom=fruit[1], color='#ff8000',
-            width=0.5, label="Oranges")
-    plt.bar(x, fruit[1], bottom=fruit[0], color='yellow',
-            width=0.5, label="Bananas")
-    plt.bar(x, fruit[0], color='red',
-            width=0.5, label="Apples")
+    # Commence à 0
+    plt.bar(x, fruit[0], color='red', width=0.5, label="Apples")
+
+    # Commence au sommet de la première (bottom = valeur de fruit[0])
+    plt.bar(x, fruit[1], bottom=fruit[0],
+            color='yellow', width=0.5, label="Bananas")
+
+    # Commence au sommet des deux autres (v de fruit[0] + fruit[1])
+    plt.bar(x, fruit[2], bottom=fruit[0] + fruit[1],
+            color='#ff8000', width=0.5, label="Oranges")
+
+    # Commence au sommet des trois autres (v de fruit[0] + fruit[1] + fruit[2])
+    plt.bar(x, fruit[3], bottom=fruit[0] + fruit[1] + fruit[2],
+            color='#ffe5b4', width=0.5, label="Peaches")
 
     plt.ylabel("Quantity of fruit")
     plt.yticks(range(0, 81, 10))
     plt.ylim(0, 80)
+    plt.title("Number of fruit per Person")
     plt.legend(loc='upper right')
     plt.show()
