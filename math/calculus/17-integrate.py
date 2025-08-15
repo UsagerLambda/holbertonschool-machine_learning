67aae5dafd08284fb17812b6e528f8d157a61a9b
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-15 08:41:46 +0200

    fix 17)integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
+"""
+calculates the integral of a polynomial
+"""
 
 
 def poly_integral(poly, C=0):
+    """
+    Args:
+        poly (list): List of integer
+        C (int, optional): integer representing the integration constant.
+        Defaults to 0.
+
+    Returns:
+        _type_: new list of coefficients representing
+        the integral of the polynomial
+    """
     if not isinstance(poly, list) or len(poly) == 0:
         return None
     if not isinstance(C, int):
+        return None
+    if not (isinstance(elem, int) for elem in poly):
         return None
 
     integral = [C]
