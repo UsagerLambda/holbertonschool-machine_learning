67e9d28581fac679cc0dc6c07fb1e674632900e5
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-19 07:21:05 +0200

    0-build_decision_tree.py

---
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """
+        Méthode récursive qui compte la profondeur max d'un arbre
+        à partir d'un nœud donné
+
+        Returns:
+            int: La plus grande valeur des résultats de la récursion
+        """
+        # Liste vide pour récupérer la profondeur max de chaque branche
+        result = []
+        # Si l'enfant gauche n'est pas vide
+        if self.left_child is not None:
+            # Recursion dans l'enfant gauche et append le resultat
+            result.append(self.left_child.max_depth_below())
+        if self.right_child is not None:  # Pareil pour la droite
+            result.append(self.right_child.max_depth_below())
+
+        if not result:
+            return 0
+
+        return max(result)  # Renvoie le plus grand résultat de la liste
+
+
+class Leaf(Node):
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        return self.depth
+
+
+class Decision_Tree():
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        return self.root.max_depth_below()
