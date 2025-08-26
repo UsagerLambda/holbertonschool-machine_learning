70c37f26d94adba3c708a6e1b013a2a7d942cff4
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-26 21:01:46 +0200

    add 10-neural_network.py

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+"""Module qui définit un réseau de neurones."""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Classe réseau de neurones à une couche cachée, binaire."""
+
+    def __init__(self, nx, nodes):
+        """
+        Neural network with one hidden layer, binary classification.
+
+        Args:
+            nx (int): number of input features
+            nodes (int): number of neurons in the hidden layer
+
+        Raises:
+            TypeError: if nx is not an integer
+            ValueError: if nx is not positive
+            TypeError: if nodes is not an integer
+            ValueError: if nodes is not positive
+        """
+        # --- Validation des parmètres ---
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # --- Private instance attributes pour couches cachées ---
+        self.__W1 = np.random.randn(nodes, nx)
+        # Vecteur de biais de la couche cachée,
+        # ayant une dimension pour chaque neurone de celle-ci (initialisé à 0)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # --- Private instance attributes pour neurons de sortie ---
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # --- getter ---
+
+    @property
+    def W1(self):
+        """Retourne la valeur de self.__W1."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Retourne la valeur de self.__b1."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Retourne la valeur de self.__A1."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Retourne la valeur de self.__W2."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Retourne la valeur de self.__b2."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Retourne la valeur de self.__A2."""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Compute forward propagation for a 2-layer neural network.
+
+        Each layer receives as input the activation values of the
+        previous layer (A[l-1]):
+            - The first layer receives the raw input X (A0 = X).
+            - Hidden layer: Z1 = W1·X + b1, A1 = sigmoid(Z1).
+            - Output layer: Z2 = W2·A1 + b2, A2 = sigmoid(Z2).
+
+        Args:
+            X (np.ndarray): input data with shape (nx, m)
+                - nx: number of features per example.
+                - m: number of examples.
+
+        Returns:
+            tuple: (A1, A2)
+                - A1 (np.ndarray): Activations from the hidden layer.
+                - A2 (np.ndarray): Activations from the output layer.
+        """
+        # --- Couche cachée ---
+        # Calcul linéaire : Z1 = W1 · X + b1
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        # Activation sigmoïde
+        self.__A1 = (1 / (1 + np.exp(-Z1)))
+
+        # --- Couche de sortie ---
+        # Reçois l'activation de la couche cachée.
+        # # Calcul linéaire : Z2 = W2 · A1 + b2
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        # Activation sigmoïde
+        self.__A2 = (1 / (1 + np.exp(-Z2)))
+
+        return self.__A1, self.__A2
