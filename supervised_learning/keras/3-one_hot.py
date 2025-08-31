8d636aa9d372a198840d92f29515a2a783a7ec07
Utilisateur Lambda <clement.chassem@gmail.com>
2025-08-31 19:30:46 +0200

    add 3-one_hot.py

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Initialize Keras model using a Adam optimization."""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+def one_hot(labels, classes=None):
+    """
+    Encode une liste de labels en représentation one-hot.
+
+    Args:
+        labels (list | np.ndarray | tf.Tensor):
+            Liste ou tableau de labels entiers (ex: [0, 1, 2]).
+            Peut aussi être un tenseur TensorFlow.
+        classes (int, optional):
+            Nombre total de classes (longueur des vecteurs one-hot).
+            Si None, sera déduit automatiquement comme max(labels) + 1.
+
+    Returns:
+        np.ndarray:
+            Tableau NumPy de forme (n_samples, classes) contenant
+            l'encodage one-hot des labels.
+    """
+    if classes is None:
+        classes = max(labels) + 1  # Trouve le nombre de classes
+    # Créer un objet qui sait transformer des entiers en vecteur one-hot
+    # num_tokens = combien de colonnes (classes)
+    # output_mode = indique le type d'encodage
+    layer = K.layers.CategoryEncoding(num_tokens=classes,
+                                      output_mode="one_hot")
+    # Utilise l'objet avec les données (labels),
+    # et convertie la sortie en tableau numpy
+    return layer(labels).numpy()
