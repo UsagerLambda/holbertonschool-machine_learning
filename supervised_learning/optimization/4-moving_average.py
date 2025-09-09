a1164297570723f2a9e50361fe5d662d66cc3cbc
Utilisateur Lambda <clement.chassem@gmail.com>
2025-09-09 09:30:47 +0200

    add 4-moving_average.py

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Calculate weighted moving average (with bias correction)."""
+
+import numpy as np
+import tensorflow as tf
+
+
+def moving_average(data, beta):
+    """
+    Calculate the weighted moving average of a data set.
+
+    Args:
+        data (list): list of data to calculate the moving average of
+        beta (float): weight used for the moving average
+    """
+    moving_average = []
+    avg = 0
+    for i in range(len(data)):
+        avg = beta * avg + (1 - beta) * data[i]  # EMA brut
+        avg_corrected = avg / (1 - beta**(i + 1))  # EMA corrigé avec biais
+        moving_average.append(avg_corrected)
+    return moving_average
