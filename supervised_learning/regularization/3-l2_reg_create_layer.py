efa0643c95a7abb400786b12ddc238f961c5a60b
Utilisateur Lambda <clement.chassem@gmail.com>
2025-09-17 11:09:25 +0200

    fix 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -17,9 +17,13 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Returns:
         keras.Tensor: output of the new layer
     """
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0, mode=("fan_avg"))
+
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
+        kernel_initializer=initializer,
         kernel_regularizer=tf.keras.regularizers.L2(lambtha)
     )
     return layer(prev)
