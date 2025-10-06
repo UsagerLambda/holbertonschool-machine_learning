dd23b3ab6bc4db366b60831c3145e2fe54d965bf
Utilisateur Lambda <clement.chassem@gmail.com>
2025-10-06 10:46:06 +0200

    add 0-inception_block.py

---
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+
+from tensorflow import keras as K
+
+
+def inception_block(A_prev, filters):
+    """
+    Construit un bloc d'inception.
+
+    Args:
+        A_prev (tensor): la sortie de la couche précédente.
+        filters (tuple ou list): contient respectivement F1, F3R, F3, F5R, F5, FPP :
+            - F1 : nombre de filtres dans la convolution 1x1.
+            - F3R : nombre de filtres dans la convolution 1x1 avant la convolution 3x3.
+            - F3 : nombre de filtres dans la convolution 3x3.
+            - F5R : nombre de filtres dans la convolution 1x1 avant la convolution 5x5.
+            - F5 : nombre de filtres dans la convolution 5x5.
+            - FPP : nombre de filtres dans la convolution 1x1 après le max pooling.
+
+    Toutes les convolutions à l'intérieur du bloc d'inception utilisent une activation ReLU (Rectified Linear Unit).
+
+    Returns:
+        tensor: la sortie concaténée du bloc d'inception.
+    """
+    F1, F3R, F3, F5R, F5, FPP = filters
+
+    # 1st layer de convolution ================================================
+
+    # F1 : convolution 1x1
+    conv_F1 = K.layers.Conv2D(
+        filters=F1,
+        kernel_size=(1, 1),
+        activation='relu',
+        padding='same',
+    )(A_prev)
+
+    # F3R : convolution 1x1 avant la convolution 3x3.
+    conv_F3R = K.layers.Conv2D(
+        filters=F3R,
+        kernel_size=(1, 1),
+        activation='relu',
+        padding='valid',
+    )(A_prev)
+
+    # F5R : convolution 1x1 avant la convolution 5x5.
+    conv_F5R = K.layers.Conv2D(
+        filters=F5R,
+        kernel_size=(1, 1),
+        activation='relu',
+        padding='valid',
+    )(A_prev)
+
+    # Pooling
+    max_pool = K.layers.MaxPooling2D(
+        pool_size=(3, 3),
+        strides=(1, 1),
+        padding='same'
+    )(A_prev)
+
+    # 2nd layer de convolution ================================================
+
+    # F3 : convolution 3x3
+    conv_F3 = K.layers.Conv2D(
+        filters=F3,
+        kernel_size=(3, 3),
+        activation='relu',
+        padding='same',
+    )(conv_F3R)
+
+    # F5 : convolution 5x5
+    conv_F5 = K.layers.Conv2D(
+        filters=F5,
+        kernel_size=(5, 5),
+        activation='relu',
+        padding='same',
+    )(conv_F5R)
+
+    # FPP : convolution 1x1
+    conv_FPP = K.layers.Conv2D(
+        filters=FPP,
+        kernel_size=(1, 1),
+        activation='relu',
+        padding='same',
+    )(max_pool)
+
+    model = K.layers.Concatenate(axis=3)([conv_F1, conv_F3, conv_F5, conv_FPP])
+    return model
