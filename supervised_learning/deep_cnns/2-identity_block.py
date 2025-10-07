a0a24c377f5d6f9a4343b886e5df1c95194d5b0f
Utilisateur Lambda <clement.chassem@gmail.com>
2025-10-07 09:50:52 +0200

    fix 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -26,12 +26,8 @@ def identity_block(A_prev, filters):
         kernel_initializer=he_init
     )(A_prev)
 
-    norm1 = K.layers.BatchNormalization(
-        axis=3,
-        epsilon=0.99,
-        momentum=0.001
-    )(conv1)
-    A = K.activations.linear(norm1)
+    norm1 = K.layers.BatchNormalization(axis=3)(conv1)
+    A = K.layers.Activation('relu')(norm1)
 
     conv2 = K.layers.Conv2D(
         filters=F3,
@@ -41,12 +37,8 @@ def identity_block(A_prev, filters):
         kernel_initializer=he_init
     )(A)
 
-    norm2 = K.layers.BatchNormalization(
-        axis=3,
-        epsilon=0.99,
-        momentum=0.001
-    )(conv2)
-    B = K.activations.linear(norm2)
+    norm2 = K.layers.BatchNormalization(axis=3)(conv2)
+    B = K.layers.Activation('relu')(norm2)
 
     conv3 = K.layers.Conv2D(
         filters=F12,
@@ -55,14 +47,9 @@ def identity_block(A_prev, filters):
         padding='same',
         kernel_initializer=he_init
     )(B)
+    norm3 = K.layers.BatchNormalization(axis=3)(conv3)
 
-    norm3 = K.layers.BatchNormalization(
-        axis=3,
-        epsilon=0.99,
-        momentum=0.001
-    )(conv3)
     C = K.layers.Add()([norm3, A_prev])
-
-    model = K.activations.linear(C)
+    model = K.layers.Activation('relu')(C)
 
     return model
