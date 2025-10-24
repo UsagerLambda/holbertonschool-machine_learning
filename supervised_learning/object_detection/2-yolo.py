c4360b9c4a36bda2e48cb04a4ba227144e0aeb16
Utilisateur Lambda <clement.chassem@gmail.com>
2025-10-24 10:18:06 +0200

    fix 2-yolo.py by removing a print debug

--- supervised_learning/object_detection/2-yolo.py
+++ supervised_learning/object_detection/2-yolo.py
@@ -61,8 +61,8 @@ class Yolo:
         else:
             input_shape = self.model.input.shape
 
-        input_height = input_shape[1]
-        input_width = input_shape[2]
+        input_height = input_shape[1] if input_shape[1] is not None else 416
+        input_width = input_shape[2] if input_shape[2] is not None else 416
 
         # Dimensions réelles de l'image
         image_height, image_width = image_size[0], image_size[1]
