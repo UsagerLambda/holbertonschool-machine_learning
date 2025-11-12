6662cf303fe448ddaef735bad14fb7d9a44f9adf
Utilisateur Lambda <clement.chassem@gmail.com>
2025-11-12 12:10:19 +0100

    fix normal.py (P0T6)

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -29,7 +29,7 @@ class Normal:
         self.stddev = float(stddev)
 
         if data is None:
-            if stddev < 0:
+            if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
 
         else:
