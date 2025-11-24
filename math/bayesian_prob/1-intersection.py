09c8719118a45b64630b8a352252a93882e3792a
Utilisateur Lambda <clement.chassem@gmail.com>
2025-11-24 14:44:49 +0100

    fix 1-intersection.py, bad raise ValueError

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -86,10 +86,11 @@ def intersection(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
-    for name, array in [P, Pr]:
-        if np.any((array < 0) | (array > 1)):
-            raise ValueError(
-                f"All values in {name} must be in the range [0, 1]")
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
 
     if not np.isclose(sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
