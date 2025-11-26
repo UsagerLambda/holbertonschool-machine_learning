527e50c2b2ceb474b582c6bd8f913fa8ca24c626
Utilisateur Lambda <clement.chassem@gmail.com>
2025-11-26 11:52:52 +0100

    update 0-pca.py - removing commentary because the checker is such a meany 3

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -25,6 +25,10 @@ def pca(X, var=0.95):
     explained_variance = (S ** 2) / np.sum(S ** 2)
     cumulative_variance = np.cumsum(explained_variance)
 
-    nd = np.argmin(cumulative_variance < var) + 1
+    indices = np.where(cumulative_variance >= var)[0]
+    if len(indices) > 0:
+        nd = indices[0] + 1
+    else:
+        nd = len(cumulative_variance)
 
     return Vt[:nd].T
