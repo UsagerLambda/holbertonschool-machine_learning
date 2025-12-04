63513600e125c142ccd03c193d9e5fe4d46414f4
Utilisateur Lambda <clement.chassem@gmail.com>
2025-12-04 19:41:26 +0100

    add 7-maximization.py

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Calculate the maximization step in the EM algorithm for a GMM."""
+
+import numpy as np
+
+
+def maximization(X, g):
+    """
+    Calculate the maximization step in the EM algorithm for a GMM.
+
+    Args:
+        X (np.ndarray): of shape (n, d) containing the data set
+        g (np.ndarray): of shape (k, n) containing the posterior
+            probabilities for each data point in each cluster
+    """
+    n, d = X.shape
+    k, _ = g.shape
+
+    # Proportion des points dans chaque cluster
+    pi = np.sum(g, axis=1) / n
+
+    m = np.zeros((k, d))
+    # Calcul le centre de chaque cluster
+    for cluster in range(k):
+        m[cluster] = np.sum(g[
+            cluster, :, np.newaxis] * X, axis=0) / np.sum(g[cluster])
+
+    # Calcul la forme des clusters
+    S = np.zeros((k, d, d))
+    for cluster in range(k):
+        diff = X - m[cluster]
+        weighted = g[
+            cluster, :, np.newaxis, np.newaxis] * (
+                diff[:, :, np.newaxis] @ diff[:, np.newaxis, :])
+        S[cluster] = np.sum(weighted, axis=0) / np.sum(g[cluster])
+
+    return pi, m, S
