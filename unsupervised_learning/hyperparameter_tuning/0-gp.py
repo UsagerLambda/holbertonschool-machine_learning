4e704bf5699137210ca8ecab3528b3383e021ae1
Utilisateur Lambda <clement.chassem@gmail.com>
2025-12-08 15:48:30 +0100

    fix 0-gp.py by rename a variable

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -7,7 +7,7 @@ import numpy as np
 class GaussianProcess:
     """Représente un processus gaussien pour l'optimisation bayésienne."""
 
-    def __init__(self, X_init, Y_init, length=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
         Initialise un processus gaussien.
 
@@ -19,13 +19,13 @@ class GaussianProcess:
                 les sorties de la fonction boîte noire pour chaque
                 entrée dans X_init
                 - t est le nombre d'échantillons initiaux
-            length (int, optional): paramètre de longueur pour le noyau
+            l (int, optional): paramètre de longueur pour le noyau
             sigma_f (int, optional): l'écart type donné
                 à la sortie de la fonction boîte noire
         """
         self.X = X_init
         self.Y = Y_init
-        self.length = length
+        self.l = l
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
 
@@ -43,4 +43,4 @@ class GaussianProcess:
         """
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(
             X2**2, 1) - 2 * np.dot(X1, X2.T)
-        return self.sigma_f**2 * np.exp(-0.5 / self.length**2 * sqdist)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
