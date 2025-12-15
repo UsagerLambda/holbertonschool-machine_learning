2449f3d50a02ad1920f046156bf6f6bbf6ea287f
Utilisateur Lambda <clement.chassem@gmail.com>
2025-12-15 16:00:42 +0100

    add 2-convolutional.py

---
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Fonction qui créer un autoencoder convolutionnel."""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, filters, latent_dims):
+    """Fonction qui créer un autoencoder convolutionnel.
+
+    Args:
+        input_dims (tuple): tuple d'entiers contenant les dimensions de
+            l'entrée du modèle
+        filters (list): liste contenant le nombre de filtres pour chaque
+            couche convolutionnelle de l'encodeur
+        latent_dims (tuple): tuple d'entiers contenant les dimensions de
+            la représentation de l'espace latent
+
+    Returns:
+        tuple: (encoder, decoder, auto)
+            encoder (keras.Model): le modèle encodeur
+            decoder (keras.Model): le modèle décodeur
+            auto (keras.Model): le modèle autoencoder complet
+    """
+    input = keras.Input(shape=input_dims)
+
+    x = keras.layers.Conv2D(
+        filters[0], (3, 3), activation='relu', padding='same')(input)
+    x = keras.layers.MaxPooling2D((2, 2))(x)
+    for dim in filters[1:]:
+        x = keras.layers.Conv2D(
+            dim, (3, 3), activation='relu', padding='same')(x)
+        x = keras.layers.MaxPooling2D((2, 2))(x)
+
+    encoder = keras.Model(inputs=input, outputs=x)
+
+    latent_input = keras.Input(shape=encoder.output.shape[1:])
+
+    x = keras.layers.Conv2DTranspose(
+        filters[-1], (3, 3), activation='relu', padding='same')(latent_input)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+
+    for dim in reversed(filters[1:-1]):
+        x = keras.layers.Conv2DTranspose(
+            dim, (3, 3), activation='relu', padding='same')(x)
+        x = keras.layers.UpSampling2D((2, 2))(x)
+
+    x = keras.layers.Conv2DTranspose(
+        filters[0], (3, 3), activation='relu', padding='valid')(x)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+    x = keras.layers.Conv2DTranspose(
+        input_dims[-1], (3, 3), activation='sigmoid', padding='same')(x)
+
+    decoder = keras.Model(inputs=latent_input, outputs=x)
+
+    auto = keras.Model(inputs=input, outputs=decoder(encoder(input)))
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
