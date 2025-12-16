54d3701b19e1c5f47fcd4be1f8ecccbbe0d80255
Utilisateur Lambda <clement.chassem@gmail.com>
2025-12-16 13:49:23 +0100

    test fix 3-variational.py

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -8,16 +8,20 @@ class Sampling(keras.layers.Layer):
     """Layer de sampling avec KL divergence."""
 
     def call(self, inputs):
+        """Layer de sampling avec KL divergence."""
         z_mean, z_log_var = inputs
-        epsilon = keras.random.normal(shape=keras.ops.shape(z_mean))
+        batch = keras.backend.shape(z_mean)[0]
+        dim = keras.backend.shape(z_mean)[1]
+        epsilon = keras.backend.random_normal(shape=(batch, dim))
 
-        kl_loss = -0.5 * keras.ops.sum(
-            1 + z_log_var - keras.ops.square(z_mean) - keras.ops.exp(z_log_var),
-            axis=1
-        )
-        self.add_loss(keras.ops.mean(kl_loss))
+        # KL divergence
+        kl_loss = -0.5 * keras.backend.sum(
+            1 + z_log_var - keras.backend.square(
+                z_mean) - keras.backend.exp(z_log_var), axis=1)
+        kl_loss = keras.backend.mean(kl_loss)
+        self.add_loss(kl_loss)
 
-        return z_mean + keras.ops.exp(0.5 * z_log_var) * epsilon
+        return z_mean + keras.backend.exp(0.5 * z_log_var) * epsilon
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
@@ -47,7 +51,8 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     z_log_var = keras.layers.Dense(latent_dims, name="z_log_var")(x)
     z = Sampling()([z_mean, z_log_var])
 
-    encoder = keras.Model(encoder_input, [z, z_mean, z_log_var], name="encoder")
+    encoder = keras.Model(
+        encoder_input, [z, z_mean, z_log_var], name="encoder")
 
     # Decoder
     latent_input = keras.Input(shape=(latent_dims,))
@@ -61,10 +66,9 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     # VAE complet
     outputs = decoder(encoder(encoder_input)[0])
     auto = keras.Model(encoder_input, outputs, name="vae")
-
-    def vae_loss(y_true, y_pred):
-        return keras.losses.binary_crossentropy(y_true, y_pred) * input_dims
-
-    auto.compile(optimizer='adam', loss=vae_loss)
+    auto.compile(
+        optimizer='adam',
+        loss='binary_crossentropy'
+    )
 
     return encoder, decoder, auto
