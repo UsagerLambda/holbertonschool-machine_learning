7a06fb7fef262b6c5e654dd2303daa759d09cf2b
Utilisateur Lambda <clement.chassem@gmail.com>
2026-01-13 16:52:29 +0100

    fix 4-deep_rnn.py

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
+"""A function that performs a forward propagation for a deep RNN."""
 
 import numpy as np
 
 
 def deep_rnn(rnn_cells, X, h_0):
+    """Perform forward propagation for a deep RNN."""
     t, m, i = X.shape
-    l = len(rnn_cells)
+    long = len(rnn_cells)
     h_prev = h_0.copy()
     H = [h_0]
     Y = []
     for step in range(t):
         x_input = X[step]
         h_new = []
-        for layer in range(l):
+        for layer in range(long):
             h_next, y = rnn_cells[layer].forward(h_prev[layer], x_input)
             h_new.append(h_next)
             x_input = h_next
