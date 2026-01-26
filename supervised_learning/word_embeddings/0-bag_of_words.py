7132d3d9cdd9e761b952591e112f2a0033eaa745
Utilisateur Lambda <clement.chassem@gmail.com>
2026-01-26 16:23:39 +0100

    add 0-bag_of_words.py

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Bag of words."""
+import string
+import numpy as np
+
+
+def bag_of_words(sentences, vocab=None):
+    """Create a bag of words embedding matrix based on a list of sentences."""
+    features = set()
+    words = []
+    embeddings = []
+
+    for sentence in sentences:
+        text = sentence.lower()
+        text = text.translate(str.maketrans("", "", string.punctuation))
+        tokens = text.split()
+        words.append(tokens)
+
+    if vocab is not None:
+        features = sorted(vocab)
+    else:
+        features = set()
+        for sentence in words:
+            features.update(sentence)
+        features = sorted(list(features))
+
+    for mots in words:
+        embed = []
+        for word in sorted(features):
+            count = mots.count(word)
+            embed.append(count)
+        embeddings.append(embed)
+
+    return np.array(embeddings), features
