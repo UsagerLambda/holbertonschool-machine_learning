1ac91b8650ffc73db1371960a75d16b0f25e182a
Utilisateur Lambda <clement.chassem@gmail.com>
2026-01-27 14:15:02 +0100

    add 4-fasttext.py

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Create, builds an trains a gensim fastText model."""
+
+import gensim
+
+
+def fasttext_model(
+            sentences,
+            vector_size=100,
+            min_count=5,
+            negative=5,
+            window=5,
+            cbow=True,
+            epochs=5,
+            seed=0,
+            workers=1
+        ):
+    """Create, builds an trains a gensim fastText model."""
+    model = gensim.models.FastText(
+            sentences=sentences,
+            vector_size=vector_size,
+            min_count=min_count,
+            window=window,
+            negative=negative,
+            sg=0 if cbow else 1,
+            epochs=epochs,
+            seed=seed,
+            workers=workers
+    )
+    model.build_vocab(sentences)
+
+    model.train(
+            sentences,
+            total_examples=model.corpus_count,
+            epochs=model.epochs
+        )
+    return model
