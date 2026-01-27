3e8424258db5fdd8ba1a03050e0586f71e06bdf3
Utilisateur Lambda <clement.chassem@gmail.com>
2026-01-27 14:09:24 +0100

    fix import 3-gensim_to_keras.py

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Gensim model to keras embedding layer."""
 
-import tensorflow
+import tensorflow as tf
 
 
 def gensim_to_keras(model):
     """Gensim model to keras embedding layer."""
     keyed_vectors = model.wv
     weights = keyed_vectors.vectors
-    layer = tensorflow.keras.layers.Embedding(
+    layer = tf.keras.layers.Embedding(
             input_dim=weights.shape[0],
             output_dim=weights.shape[1],
             weights=[weights],
