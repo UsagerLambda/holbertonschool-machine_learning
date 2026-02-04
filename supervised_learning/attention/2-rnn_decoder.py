ebb917e0fa6760c35a5c4516f097388a3d4b31a6
Utilisateur Lambda <clement.chassem@gmail.com>
2026-02-04 10:12:32 +0100

    fix 2-rnn_decoder.py

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -2,7 +2,7 @@
 """Module contenant le décodeur RNN avec attention."""
 
 import tensorflow as tf
-SelfAttention = __import__("1-self_attention").SelfAttention
+SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
