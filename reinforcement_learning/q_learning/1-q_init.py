c08dcdf7ce678ef368c6cf999bb208b1b837253a
Utilisateur Lambda <clement.chassem@gmail.com>
2026-03-02 11:03:51 +0100

    add 1-q_init.py

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Initialize the Q-table of the environnement."""
+
+import numpy as np
+
+def q_init(env):
+    """Initialize the Q-table of the environnement."""
+    Qtable = np.zeros((env.observation_space.n, env.action_space.n))
+    return Qtable
