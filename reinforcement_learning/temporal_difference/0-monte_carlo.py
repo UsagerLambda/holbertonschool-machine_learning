55666beadba37cdb1383a48e6e576e081fcc8ac0
Utilisateur Lambda <clement.chassem@gmail.com>
2026-03-17 12:08:28 +0100

    fix P0T0

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -5,18 +5,21 @@ import numpy as np
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
     for episode in range(episodes):
-        state, _ = env.reset()
+        state = 0
+        env.reset()
         episode_data = []
 
         for step in range(max_steps):
             action = policy(state)
             new_state, reward, terminated, truncated, _ = env.step(action)
             episode_data.append((state, reward))
-            state = new_state
             if terminated or truncated:
                 break
+            state = new_state
+        episode_data = np.array(episode_data, dtype=int)
         G = 0
         for state, reward in reversed(episode_data):
             G = reward + gamma * G
-            V[state] = V[state] + alpha * (G - V[state])
+            if state not in episode_data[:episode, 0]:
+                V[state] = V[state] + alpha * (G - V[state])
     return V
