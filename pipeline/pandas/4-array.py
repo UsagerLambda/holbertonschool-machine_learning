2f59b3cdc117563d524dfb79d01d7b7256b21258
Utilisateur Lambda <clement.chassem@gmail.com>
2026-04-02 10:02:02 +0200

    add 4-array.py

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Convert les 10 dernier rows High & Close du DF en tableau numpy."""
+
+import pandas as pd
+
+
+def array(df):
+    """Convert les 10 dernier rows High & Close du DF en tableau numpy."""
+    df = df[["High", "Close"]]
+    df = df.tail(10)
+    df = df.to_numpy()
+    return df
