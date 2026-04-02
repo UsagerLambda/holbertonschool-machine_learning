8e711f3f2facc2b80e4c41f878d231cd885b530e
Utilisateur Lambda <clement.chassem@gmail.com>
2026-04-02 09:10:18 +0200

    add 1-from_dictionnary.py

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Créer un DataFrame depuis un dictionnaire."""
+
+import pandas as pd
+import numpy as np
+
+
+df = pd.DataFrame(
+    {
+        "First": np.array([0.0, 0.5, 1.0, 1.5]),
+        "Second": np.array(["one", "two", "three", "four"]),
+    },
+    index=list("ABCD"),
+)
